e87f7f811d87d5a392c340ecc035053390f49c90
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2010-02-28 20:58:37 -0500

    Fix down migration for test app

--- test/app_root/db/migrate/004_migrate_preferences_to_version_1.rb
+++ test/app_root/db/migrate/004_migrate_preferences_to_version_1.rb
@@ -6,7 +6,7 @@ class MigratePreferencesToVersion1 < ActiveRecord::Migration
   end
   
   def self.down
-    ActiveRecord::Migrator.new(:up, "#{Rails.root}/../../generators/preferences/templates", 0).migrations.each do |migration|
+    ActiveRecord::Migrator.new(:down, "#{Rails.root}/../../generators/preferences/templates", 0).migrations.each do |migration|
       migration.migrate(:down)
     end
   end
