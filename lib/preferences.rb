fc207ee06340f9760f8f258daa8f6b445941377b
Jack Dempsey <jack.dempsey@gmail.com>
2011-05-18 22:50:09 -0400

    make preferences an engine so it loads models in app/models

--- lib/preferences.rb
+++ lib/preferences.rb
@@ -1,3 +1,4 @@
+require 'preferences/engine'
 require 'preferences/preference_definition'
 
 # Adds support for defining preferences on ActiveRecord models.
---
+++ lib/preferences/engine.rb
@@ -0,0 +1,4 @@
+module Preferences
+  class Engine < Rails::Engine
+  end
+end
